dbeef5999ff884c37af26f6e7c0a514bc48631ba
minjae.lee <stkment@gmail.com>
2021-11-28 01:16:30 +0900

    [week-1] Styling and theming your app

--- week 1-Jetpack Compose basics/ComposeBasicsCodeLab/app/src/main/java/com/jay/composebasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasicsCodeLab/app/src/main/java/com/jay/composebasicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jay.composebasicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -115,10 +116,16 @@ private fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeBasicsCodeLabTheme {
-        MyApp()
+        Greetings()
     }
 }
