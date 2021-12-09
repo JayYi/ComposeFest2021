d82bd27e2e99cb131af254121dd17a6396554240
minjae.lee <stkment@gmail.com>
2021-12-09 23:44:09 +0900

    [week-3] Working with Shapes

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -168,7 +169,7 @@ private fun PostMetadata(
             }
             append(" ${tag.uppercase(Locale.getDefault())} ")
             withStyle(tagStyle) {
-                append(" ${tag.toUpperCase()} ")
+                append(" ${tag.uppercase()} ")
             }
         }
     }
@@ -193,6 +194,7 @@ fun PostItem(
         icon = {
             Image(
                 painter = painterResource(post.imageThumbId),
+                modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
                 contentDescription = null
             )
         },
